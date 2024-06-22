5ccf85a786b9a9770943a836a339a11e1bf14937
Brian Carmichael <bdonjr7@gmail.com>
2024-06-22 11:41:51 -0700

    users updated

--- project/app/seeds/users.py
+++ project/app/seeds/users.py
@@ -5,15 +5,15 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', first_name='Demo', last_name='User', email='demo@aa.io', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', first_name='Marnie', last_name='Doe', email='marnie@aa.io', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', first_name='Bobbie', last_name='Doe', email='bobbie@aa.io', password='password')
+    brian = User(
+        username='brian', first_name='Brian', last_name='Carmichael', email='brian@aa.io', password='password')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add_all([demo, marnie, bobbie, brian])
     db.session.commit()
 
 
@@ -28,5 +28,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
