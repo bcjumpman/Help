668ca1a786c197ceb18adb6b4c402c2ec46793eb
Brian Carmichael <bdonjr7@gmail.com>
2024-06-25 16:44:56 -0700

    biz model done

--- project/app/models/business.py
+++ project/app/models/business.py
@@ -4,3 +4,59 @@ from sqlalchemy.orm import relationship
 
 class Business (db.Model):
     __tablename__ = 'businesses'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
+        id = Column(Integer, primary_key=True)
+        owner_id = Column(Integer, ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+        title = Column(String(255), nullable=False)
+        address = Column(String(255), nullable=False)
+        city = Column(String(255), nullable=False)
+        state = Column(String(255), nullable=False)
+        country = Column(String(255), nullable=False)
+        price_rating = Column(Integer, nullable=False)
+        category = Column(String(255), nullable=False)
+        lat = Column(Float)
+        lng = Column(Float)
+        phone_number = Column(String(25), nullable=False)
+        description = Column(String(2000))
+        schedule = Column(String(500), nullable=True)
+
+
+        users = relationship('User', back_populates='businesses')
+        menus = relationship('Menu', back_populates='businesses', cascade='all, delete-orphan')
+        amenities = relationship('Amenity', back_populates='businesses', cascade='all, delete-orphan')
+        reviews = relationship('Review', back_populates='businesses', cascade='all, delete-orphan')
+        business_images = relationship('BusinessImage', back_populates='businesses', cascade='all, delete-orphan')
+
+        @property
+        def business_img(self):
+            return [images.to_dict() for images in self.business_images]
+
+        @property
+        def business_reviews(self):
+            return [review.to_dict() for review in self.reviews]
+
+        @property
+        def business_menu(self):
+            return [menu.to_dict() for menu in self.menus]
+
+        def to_dict(self):
+            return {
+                'id': self.id,
+                'owner_id': self.owner_id,
+                'title': self.title,
+                'address': self.address,
+                'city': self.city,
+                'state': self.state,
+                'country': self.country,
+                'price_rating': self.price_rating,
+                'category': self.category,
+                'lat': self.lat,
+                'lng': self.lng,
+                'phone_number': self.phone_number,
+                'description': self.description,
+                'schedule': self.schedule,
+            }
